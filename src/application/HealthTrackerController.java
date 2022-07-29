e045f77a7b250e251c40c574b4625b29580bdee8
rajao <rajao@10.0.0.239>
2022-07-28 20:28:48 -0600

    Changed root of scene 1 to VBox and added code to validate user inputs in scene 1

--- src/application/HealthTrackerController.java
+++ src/application/HealthTrackerController.java
@@ -10,66 +10,99 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 
 public class HealthTrackerController {
-	
-	 @FXML 
-	 private TextField nameTextField;
-	 
-	 @FXML 
-	 private ChoiceBox<String> genderChoiceBox;
-	  
-	 @FXML 
-	 private Label introductionLabel;
 
-	 @FXML 
-	 private TextField ageTextField;
+	@FXML
+	private TextField nameTextField;
 
-	 @FXML 
-	 private TextField currentweightTextField;
+	@FXML
+	private ChoiceBox<String> genderChoiceBox;
 
-	 @FXML 
-	 private TextField heightTextField;
-	 
-	 @FXML 
-	 private ChoiceBox<String> activityChoiceBox;
-	 
-    
+	@FXML
+	private Label introductionLabel;
+
+	@FXML
+	private TextField ageTextField;
+
+	@FXML
+	private TextField currentweightTextField;
+
+	@FXML
+	private TextField heightTextField;
+
+	@FXML
+	private ChoiceBox<String> activityChoiceBox;
+
+	@FXML
+	private Label validName;
+
+	@FXML
+	private Label validGender;
+
+	@FXML
+	private Label errorInputs;
+
 	private Stage stage;
 	private Scene scene;
 	private VBox root;
-	
+
 	public void switchToScene2(ActionEvent event) throws IOException {
+
+		validName.setText("");
+		String name = nameTextField.getText();
 		root = FXMLLoader.load(getClass().getResource("Scene_2.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		if (name == "" && genderChoiceBox.getValue() == null) {
+			errorInputs.setTextFill(Color.color(1, 0, 0));
+			errorInputs.setText("Please enter your name and pick a gender");
+		} else {
+			errorInputs.setText("");
+			if (name.matches("[a-zA-Z]+")) {
+				if (genderChoiceBox.getValue() != null) {
+					scene = new Scene(root);
+					stage.setScene(scene);
+					stage.show();
+				} else {
+					validName.setText("");
+					validGender.setTextFill(Color.color(1, 0, 0));
+					validGender.setText("Please pick a gender");
+
+				}
+			} else {
+				validGender.setText("");
+				validName.setTextFill(Color.color(1, 0, 0));
+				validName.setText("Please enter a valid name");
+			}
+
+		}
+	}
+
+	public void switchToScene3(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("Scene_3.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
 	}
-	
-	public void switchToScene3(ActionEvent event) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("Scene_3.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();	
-		}
-	
+
 	public void switchToScene4(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("Scene_4.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
-		stage.show();		
-		}
-	
+		stage.show();
+	}
+
 	public void switchToScene5(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("Scene_5.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
-		stage.show();		
-		}
+		stage.show();
+	}
 }
