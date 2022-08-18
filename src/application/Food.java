e704f25e2183644fee4e2cb43877c5d2037dc200
Raja Muhammed Omar <109646591+rajaomar@users.noreply.github.com>
2022-08-17 20:43:47 -0600

    refactored Nutrient to Nutrients

--- src/application/Food.java
+++ src/application/Food.java
@@ -6,6 +6,6 @@ public abstract class Food {
     /**
      * Food base class which can be inherited by any food
      */
-    protected HashMap<String,Nutrient> Food_Nutrients;
+    protected HashMap<String,Nutrients> Food_Nutrients;
     abstract int[] calculateCalories(Breakfast breakfast, Lunch lunch, Dinner dinner);
 }
